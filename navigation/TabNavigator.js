fa962e7c722e4cd08a02c017005367ea7b83395f
Sreshta2006 <70201942+Sreshta2006@users.noreply.github.com>
2021-09-27 22:53:53 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Feed from '../screens/Feed';
+import CreatePost from '../screens/CreatePost';
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Feed') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Create Post') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          }
+          return <Ionicons name={iconName} size={25} color={color} />;
+        },
+      })}
+      activeColor={'#ee8249'}
+      inactiveColor={'gray'}>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Create Post" component={CreatePost} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: 'white',
+  },
+});
+
+export default BottomTabNavigator;
